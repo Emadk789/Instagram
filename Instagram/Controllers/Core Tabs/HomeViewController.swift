655d4c7f84d8b2fc8384b40c202255cb981f06f5
Emad Albarnawi <Emadk789@hotmail.com>
2021-02-10 19:33:05 +0300

    chore: Setup horizontalCollectionView in HomeViewController.

--- Instagram/Controllers/Core Tabs/HomeViewController.swift
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -10,16 +10,41 @@ import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
+    private let horizontalCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.estimatedItemSize = CGSize(width: 60,
+                                          height: 60)
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         tabBarController?.tabBar.tintColor = UIColor.label
-        // Do any additional setup after loading the view.
+        
+        addSubViews()
+        configurHorizantalCollectionViewAnchors()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         handelNotAuthenticated()
+        horizontalCollectionView.delegate = self
+        horizontalCollectionView.dataSource = self
+    }
+    private func addSubViews() {
+        view.addSubview(horizontalCollectionView)
+    }
+    private func configurHorizantalCollectionViewAnchors() {
+        horizontalCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        horizontalCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        horizontalCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        horizontalCollectionView.heightAnchor.constraint(equalToConstant: 60).isActive = true
     }
     
     private func handelNotAuthenticated() {
@@ -33,4 +58,22 @@ class HomeViewController: UIViewController {
             
         }
     }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        cell.layer.cornerRadius = cell.width/2
+        return cell
+    }
+    
+    
 }
