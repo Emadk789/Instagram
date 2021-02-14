1edbe63783c67587e6165508864856bd33498917
Emad Albarnawi <Emadk789@hotmail.com>
2021-02-14 21:26:19 +0300

    Setup The StorageManager.

--- Instagram/Resorces/StorageManager.swift
+++ Instagram/Resorces/StorageManager.swift
@@ -8,11 +8,35 @@
 import Foundation
 import FirebaseStorage
 
+
 class StorageManager {
     static let shared = StorageManager()
     
+    private let bucket = Storage.storage().reference()
     private init() {}
     
-
+    enum IGStoregManagerError: Error {
+        case faildToDownload
+    }
     
+    func uploadUserPost(model: UserPost, completion: @escaping (Result<URL, Error>) -> Void) {
+        
+    }
+    func downloadImage(with reference: String, completion: @escaping (Result<URL, IGStoregManagerError>) -> Void) {
+        bucket.child(reference).downloadURL { (url, error) in
+            guard url != nil, error == nil else {
+                completion(.failure(.faildToDownload))
+                return
+            }
+            
+            completion(.success(url!))
+        }
+    }
+}
+
+enum UserPostType {
+    case photo, video
+}
+struct UserPost {
+    let postType: UserPostType
 }
